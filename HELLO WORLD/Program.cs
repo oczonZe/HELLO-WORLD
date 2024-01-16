b6fc47bd5b6627de968451aa648f127ef8281908
zemue <zemue@DESKTOP-9MF3GHP>
2024-01-16 10:32:16 +0800

    Add project files.

---
+++ HELLO WORLD/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HELLO_WORLD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
